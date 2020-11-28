87c834ac212f1ff876cfeaeaf423be70f7ea11fd
Nika <veronikanatykanaya@gmail.com>
2020-11-28 11:22:42 +0200

    new inst

--- bot.py
+++ bot.py
@@ -9,18 +9,7 @@ bot = telebot.TeleBot(TOKEN)
 def start_handler(message):
     bot.send_message(message.chat.id,'Привет, маленький засранец :)')
 
-# @bot.message_handler(commands=['help'])
-# def any_message(message):
-#     caption = 'Какие глазки! :eyes:'
-#     bot.reply_to(message, caption.format(message.text))
-#
 
-# @bot.message_handler(commands=['photo'])
-# def process_photo_command(message):
-#     caption = 'Какие глазки! :eyes:'
-#     bot.reply_to(message.from_user.id,
-#                          caption=emojize(caption),CAT_BIG_EYES,
-#                          reply_to_message_id=message.message_id)
 
 @bot.message_handler(content_types=['sticker'])
 def sticker_id(message):
@@ -30,22 +19,13 @@ def sticker_id(message):
 def send_text(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.chat.id, 'Привет, мой создатель')
+
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Прощай, создатель')
-    #elif message.text.lower() == 'я тебя люблю':
-     #   bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBD9ZfEJCVXn_KSm7COrSSzFY15DU3CAACdQEAAnvAfRNZSgGUv-3IZhoE')
+
     elif message.text.lower() == 'фейсбук':
         bot.send_message(message.chat.id, fcbook())
-   # elif message.text.lower() == 'лина':
-     #   bot.send_message(message.chat.id, 'Лина, йди у сраку')
-    #elif message.text.lower() == 'английские маты':
-      #  bot.send_message(message.chat.id, '\nAss \nFaggot\nDick sucker\nHooker')
-   # elif message.text.lower() == 'русские маты':
-      #  bot.send_message(message.chat.id, '\nЖопа \nМудак\nЧленосос\nШлюха')
-    #elif message.text.lower() == 'итальянские маты':
-      #  bot.send_message(message.chat.id, '\nCulo \nStronzo\nSucchiacazzi\nPuttana')
-    #elif message.text.lower() == 'немецкие маты':
-       # bot.send_message(message.chat.id, '\nArsch \nArschloch\nSchwanzlutscher\nHure')
+
     else:
         bot.send_message(message.chat.id, 'а всё уже')
 @bot.message_handler(func=lambda message: True)
--- test.py
+++ test.py
@@ -3,34 +3,25 @@ import requests
 import time
 
 def fcbook():
-    #вытащила подписчиков
     r = requests.get("https://www.facebook.com/ProgresstechUA/")
-    # the script extracts all website andbo return in
     soup = bs(r.text, "html.parser")
-    #podsp_p=[]
-    # проверяем существование вакансий по запросу (если есть этот тег, то значит вакансий нет)
     podsp = soup.find_all('div', class_="_4bl9")
     #podsp_p=podsp.find('div')
     # for i in podsp:
     #    print(i.get_text())
     podsp_p=podsp[2]
-    #print(podsp_p.get_text())
     return podsp_p.get_text()
 
 def inst():
     r = requests.get("https://www.instagram.com/progresstech.ua/?hl=ru")
-    # the script extracts all website andbo return in
     soup = bs(r.text, "html.parser")
-    #podsp_pa=[]
-    # проверяем существование вакансий по запросу (если есть этот тег, то значит вакансий нет)
-    podpis = soup.find_all('span', class_="g47SY ")
-    if podpis==None:
-        print("Nooonee")
-    else:
-        print("no none!")
-    print(podpis)
+    podpis = soup.find('meta',property="og:description")
+    #
+    podp=podpis["content"]
+    podpp=podp.split()
+
+    print(podpp[0],podpp[1])
+    #текущее время
     #print(time.ctime())
-    for i in podpis:
-        print(i.get_text())
 
 inst()
