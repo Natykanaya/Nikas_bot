40949377890cdbd52e5542c76a14abe1e5338a9e
Nika <veronikanatykanaya@gmail.com>
2020-12-02 00:20:54 +0200

    proxy

--- test.py
+++ test.py
@@ -22,9 +22,40 @@ def inst():
     podp=podpis["content"]
     podpp=podp.split()
 
-    ret = podpp[0] +" "+ podpp[1]
+    ret = podpp[0] +" "+ podpp[1].replace(',','')
+    # for i in ret:
+    #print(ret)
     return ret
     #текущее время
     #print(time.ctime())
+def vk():
+    print("try")
 
-inst()
+    # proxies = {
+    #     'http': 'http://10.10.1.10:3128',
+    #     'https': 'http://10.10.1.10:1080',
+    # }
+
+
+
+    # proxies = {'http://10.20.1.128': 'http://10.10.1.10:5323'}
+
+
+
+
+    # r = requests.get("https://vk.com/progresstech_ukraine",
+    #                  proxies={"http": "http://61.233.25.166:80"})
+
+
+    # s = requests.Session()
+    # s.proxies = {"http": "http://61.233.25.166:80"}
+    #
+    # r = s.get("https://vk.com/progresstech_ukraine")
+    # print(r.text)
+    #print(r.text)
+
+
+    #r=requests.get('https://vk.com/progresstech_ukraine', proxies=proxies)
+    # soup = bs(r.text, "html.parser")
+    # print (soup)
+vk()
