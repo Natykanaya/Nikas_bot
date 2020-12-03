ba855426aaec9ee48204d11cd1ef1f4f9f34c58f
Nika <veronikanatykanaya@gmail.com>
2020-12-03 13:30:04 +0200

    proxy

--- test.py
+++ test.py
@@ -1,61 +1,33 @@
 from bs4 import BeautifulSoup as bs
 import requests
-import time
 
 def fcbook():
     r = requests.get("https://www.facebook.com/ProgresstechUA/")
     soup = bs(r.text, "html.parser")
     podsp = soup.find_all('div', class_="_4bl9")
-    #podsp_p=podsp.find('div')
-    # for i in podsp:
-    #    print(i.get_text())
+    num_list = []
+
     podsp_p=podsp[2]
-    return podsp_p.get_text()
-
-
+    subs=podsp_p.get_text()
+    for word in subs:
+        if word.isnumeric():
+            num_list.append(word)
+    #убираем скобки списка и выводим цифру
+    subscribers=''.join(num_list)
+    return subscribers
 
 def inst():
     r = requests.get("https://www.instagram.com/progresstech.ua/?hl=ru")
     soup = bs(r.text, "html.parser")
     podpis = soup.find('meta',property="og:description")
-    #
     podp=podpis["content"]
     podpp=podp.split()
-
-    ret = podpp[0] +" "+ podpp[1].replace(',','')
-    # for i in ret:
-    #print(ret)
+    ret = podpp[0]
     return ret
-    #текущее время
-    #print(time.ctime())
+
 def vk():
     print("try")
 
-    # proxies = {
-    #     'http': 'http://10.10.1.10:3128',
-    #     'https': 'http://10.10.1.10:1080',
-    # }
-
-
-
-    # proxies = {'http://10.20.1.128': 'http://10.10.1.10:5323'}
-
-
-
-
-    # r = requests.get("https://vk.com/progresstech_ukraine",
-    #                  proxies={"http": "http://61.233.25.166:80"})
-
-
-    # s = requests.Session()
-    # s.proxies = {"http": "http://61.233.25.166:80"}
-    #
-    # r = s.get("https://vk.com/progresstech_ukraine")
-    # print(r.text)
-    #print(r.text)
-
-
-    #r=requests.get('https://vk.com/progresstech_ukraine', proxies=proxies)
-    # soup = bs(r.text, "html.parser")
-    # print (soup)
-vk()
+    r=requests.get("https://vk.com/progresstech_ukraine")
+    soup = bs(r.text, "html.parser")
+    print (soup)
